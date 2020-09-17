aa7fad1724ef06c4afc7454d64372a65da67f66e
chenwangnec <chenwangnec@163.com>
2020-09-17 22:44:47 +0800

    Update sogou.sh

--- sogou.sh
+++ sogou.sh
@@ -1,4 +1,4 @@
 sudo apt update
 sudo apt install gdebi -y
 wget http://cdn2.ime.sogou.com/dl/index/1599192613/sogoupinyin_2.3.2.07_amd64-831.deb
-sudo gdebi sogoupinyin_2.3.2.07_amd64-831.deb 
+yes|sudo gdebi sogoupinyin_2.3.2.07_amd64-831.deb 
