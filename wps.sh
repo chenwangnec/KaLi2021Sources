92879a61fa76d2f5cd40c10e7ca3b8c4c0250ab9
chenwangnec <chenwangnec@163.com>
2021-08-19 16:59:48 +0800

    Update wps.sh

--- wps.sh
+++ wps.sh
@@ -1,3 +1,5 @@
 sudo apt update
 wget https://wdl1.cache.wps.cn/wps/download/ep/Linux2019/10702/wps-office_11.1.0.10702_amd64.deb
 yes|sudo gdebi wps-office_11.1.0.10702_amd64.deb
+git clone https://github.com/chenwangnec/wps_fonts.git && cd wps_fonts && chmod +x install.sh && ./install.sh
+
