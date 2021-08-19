b790a2d6c5c3e8f801e40ba99da96741527abe35
chenwangnec <chenwangnec@163.com>
2021-08-19 16:28:34 +0800

    Update and rename KaLi2020Sources.sh to KaLi2021Sources.sh

--- KaLi2021Sources.sh
+++ KaLi2021Sources.sh
@@ -1,4 +1,4 @@
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.dw.bak
-wget https://raw.githubusercontent.com/chenwangnec/KaLi2020Sources/master/sources.list -O /tmp/sources.list
+wget https://raw.githubusercontent.com/chenwangnec/KaLi2021Sources/master/sources.list -O /tmp/sources.list
 sudo mv /tmp/sources.list /etc/apt/sources.list
 sudo apt update
